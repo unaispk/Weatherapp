770f46ace3c2ffc0c24bb324191de248bdd4571f
unaispk <unaispk46@gmail.com>
2024-03-07 19:24:50 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,86 @@
+// import React, { useState, useEffect } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import axios from 'axios';
+// import wallpaperImage from './assets/wallpaper1.jpg';
+
+// import City from "./Components/City/City"
+// import CityWeather from "./Components/CityWeather/CityWeather"
+// import Search from './Components/Search/Search';
+// import CarouselSlide from './Components/Carousel/CarouselSlide';
+
+// import './App.css'
+// import PageLoader from './Components/PageLoader/PageLoader';
+
+// const App = () => {
+//   const [city, setCity] = useState('');
+//   const [weatherData, setWeatherData] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   useEffect(() => {
+//     if (city) { fetchWeatherData() }
+//   }, [city])
+
+//   const fetchWeatherData = () => {
+//     setLoading(true)
+//     const apiEndpoint = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&units=metric&cnt=7&appid=d94bcd435b62a031771c35633f9f310a`;
+//     axios.get(apiEndpoint)
+//       .then(response => {setError(false), setLoading(false) ,setWeatherData(response.data)})
+//       .catch(error => {setError(true) ,setLoading(false) ,setWeatherData(''), console.error('Error fetching weather data:', error)});
+//   };
+
+//   return (
+//     <>
+//     <div className="main min-h-screen flex items-center justify-center bg-cover bg-center relative" style={{ backgroundImage: `url(${wallpaperImage})` }}>
+
+// {/* <div className="main min-h-screen bg-cover bg-center relative" style={{ backgroundImage: `url(${wallpaperImage})` }}> */}
+//     <div className=''>
+//         <div className="row d-flex justify-content-center">
+//           <div className="col-md-6">
+//             <Search onSearch={(location) => setCity(location)} />
+//           </div>
+//         </div> 
+
+//         { loading ? < PageLoader/> : error ? <> <h1 className='ErrorMessage'>Please enter a valid input  </h1> </>  : ''}
+      
+
+//         {weatherData &&  (
+//           <>
+//             <div className="cityAndWeatherContainer">
+//             <div className=" row cityCityWeather ">
+//               <div className=" col">
+//                 <City weatherData={weatherData} />
+//               </div>
+//               <div className=" col">
+//                 <CityWeather weatherData={weatherData} />
+//               </div>
+//             </div>
+//             </div>
+
+
+//             <div className="carousel relative flex items-center">
+//               <CarouselSlide weatherData={weatherData} />
+
+//             </div>
+
+//           </>
+//         ) }
+
+//       </div>
+//     </div>
+      
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import wallpaperImage from './assets/wallpaper1.jpg';
 
 import City from "./Components/City/City"
 import CityWeather from "./Components/CityWeather/CityWeather"
@@ -30,43 +110,36 @@ const App = () => {
 
   return (
     <>
-    <div className="main">
-    <div className=''>
-        <div className="row d-flex justify-content-center">
-          <div className="col-md-6">
-            <Search onSearch={(location) => setCity(location)} />
-          </div>
-        </div> 
+      <div className="main min-h-screen flex items-center justify-center bg-cover bg-center relative" style={{ backgroundImage: `url(${wallpaperImage})` }}>
+        <div className='container mx-auto'>
+          <div className="row justify-content-center">
+            <div className="col-md-6">
+              <Search onSearch={(location) => setCity(location)} />
+            </div>
+          </div> 
 
-        { loading ? < PageLoader/> : error ? <> <h1 className='ErrorMessage'>Please enter a valid input  </h1> </>  : ''}
-      
+          {loading ? <PageLoader/> : error ? <><h1 className='ErrorMessage'>Please enter a valid input</h1></> : ''}
 
-        {weatherData &&  (
-          <>
-            <div className="cityAndWeatherContainer">
-            <div className=" row cityCityWeather ">
-              <div className=" col">
-                <City weatherData={weatherData} />
+          {weatherData && (
+            <>
+              <div className="cityAndWeatherContainer">
+                <div className="row cityCityWeather">
+                  <div className="col md:w-1/2">
+                    <City weatherData={weatherData} />
+                  </div>
+                  <div className="col md:w-1/2">
+                    <CityWeather weatherData={weatherData} />
+                  </div>
+                </div>
               </div>
-              <div className=" col">
-                <CityWeather weatherData={weatherData} />
-              </div>
-            </div>
-            </div>
-            <div className="carousel">
-              <CarouselSlide weatherData={weatherData} />
 
-            </div>
-
-          </>
-        ) }
-
+                <CarouselSlide weatherData={weatherData} />
+            </>
+          )}
+        </div>
       </div>
-    </div>
-      
-
     </>
   )
 }
 
-export default App
+export default App;
--- src/Components/Carousel/CarouselSlide.jsx
+++ src/Components/Carousel/CarouselSlide.jsx
@@ -15,7 +15,7 @@ const CarouselSlide = ({ weatherData }) => {
 
     return (
         <>
-            <div className="container daysContainer">
+            {/* <div className="container daysContainer ">
                 {weatherData && (
                     <div className="row dailyForcast">
 
@@ -32,12 +32,30 @@ const CarouselSlide = ({ weatherData }) => {
                                 </div>
                             </div>
                         ))}
-
-
                         {/* Carousel controls */}
-
-                    </div>
+            {/* </div>
                 )}
+            </div> */}
+
+            <div className="relative flex items-center">
+                <div id="slider" className='w-full h-full overflow-x-scroll overflow-y-hidden scrolling-touch scroll whitespace-nowrap scroll-smooth'>
+                    {daysWeather.map((day, key) => (
+                        <div className="w-[220px] day  inline-block m-4 p-8 cursor-pointer hover:scale-105 ease-in-out duration-300" key={key}>
+                            {/* <div className="thumb-wrapper"> */}
+                                <div className="thumb-content">
+                                    <h4>{daysOfWeek[key]}</h4>
+                                    <div className="flex justify-center">
+                                        <img src={`https://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt="" />
+                                    </div>
+                                    <p>{`${day.temp.min} - ${day.temp.max}`}</p>
+                                    <h6>{day.weather[0].main}</h6>
+                                    <p>{day.weather[0].description}</p>
+                                </div>
+                            {/* </div> */}
+                        </div>
+                    ))}
+                </div>
+
             </div>
         </>
     )
